a6123c1a9f1df347259b6f73ef7300a55eeed602
Raul Rodriguez <raurudesulml@gmail.com>
2023-12-05 21:05:54 -0600

    Se utilizo la base de datos en memoria para verificar

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Curso_Entity_Framework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+    dbContext.Database.EnsureCreated();//asegura que la DB este creado o no
+    return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+
+});
 
 app.Run();

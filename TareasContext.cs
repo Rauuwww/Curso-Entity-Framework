255010a7248416467f18925b642674db77e88dd7
Raul Rodriguez <raurudesulml@gmail.com>
2023-12-05 20:21:00 -0600

    se agrego la comfiguracion de EF

---
+++ TareasContext.cs
@@ -0,0 +1,13 @@
+﻿using Curso_Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso_Entity_Framework
+{
+    public class TareasContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+        public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+       
+    }
+}
